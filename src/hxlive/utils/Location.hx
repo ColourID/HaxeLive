1d8651f6febcef171e61de2ff6e984c6056eed71
brutalexcess <selman.luke1@gmail.com>
2016-01-08 12:11:38 +0000

    More corrections

--- src/hxlive/utils/Location.hx
+++ src/hxlive/utils/Location.hx
@@ -32,26 +32,26 @@ class Location
     
     public static inline function screenFromTop(obj:DisplayObject, offset:Float = 0)
 	{
-        obj.x = Lib.current.stage.stageWidth / 2 - obj.width;
+        obj.x = Lib.current.stage.stageWidth / 2 - obj.width / 2;
         obj.y = offset;
 	}
     
     public static inline function screenFromLeft(obj:DisplayObject, offset:Float = 0)
     {
         obj.x = offset;
-        obj.y = Lib.current.stage.stageHeight / 2 - obj.height;
+        obj.y = Lib.current.stage.stageHeight / 2 - obj.height / 2;
     }
     
     public static inline function screenFromRight(obj:DisplayObject, offset:Float = 0)
     {
         obj.x = obj.width + offset - Lib.current.stage.stageWidth;
-        obj.y = Lib.current.stage.stageHeight / 2 - obj.height;
+        obj.y = Lib.current.stage.stageHeight / 2 - obj.height / 2;
     }
     
     public static inline function screenFromBottom(obj:DisplayObject, offset:Float = 0)
     {
         obj.y = obj.height + offset - Lib.current.stage.stageHeight;
-        obj.x = Lib.current.stage.stageWidth / 2 - obj.width;
+        obj.x = Lib.current.stage.stageWidth / 2 - obj.width / 2;
     }
     
 }
